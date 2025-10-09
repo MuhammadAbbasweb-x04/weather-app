ef3035a20b14aeaa1bddea7ee928c55275999504
khalilsediq <khalilullahsadique1334@gmail.com>
2025-10-09 14:12:47 +0500

    added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
-const apiurl = "http://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=Karachi";
-const apikey = "http://api.weatherapi.com/v1/current.json?key=abcd1234xyz5678&q=Karachi";
+const apiurl = "https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=Lahore";
+const apikey = "7b636e27f20645f4b4c90819250910";
 
 
 axios(apiurl).then(res =>{
 console.log(res.data);
+console.log("hello");
 
 
 }).catch(err =>{
