50a60180d4151040bd631a07c76a33f2c3644f82
khalilsediq <khalilullahsadique1334@gmail.com>
2025-10-09 18:15:52 +0500

    added

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 
-// let  url = "https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=Lahore";
-// let apikey = "";
 let container = document.querySelector(".container");
 
-let  cite = document.querySelector("#cite");
-let form = document.querySelector("form");
+let  input = document.querySelector("#city");
+let form = document.querySelector(".input-group");
 
 form.addEventListener("submit",  (event)=>{
     event.preventDefault();
-axios(`https://api.weatherapi.com/v1/current.json?key=7b636e27f20645f4b4c90819250910=${cite.value}`).then(res =>{
+axios(
+    `https://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${city.value}&aqi=no`
+  ).then((res) =>{
     console.log(res.data);
-container.innerHTML = `  <div class="purple-weather-card">
-    <div class="city">Lahore, Pakistan</div>
-    <div class="temp">28°C</div>
+container.innerHTML = `<div class="purple-weather-card">
+            <h1>${res.data.location.name}</h1>
+    <div class="city">${res.data.location.localtime} ,${res.data.location.country}</div>
+    <div class="temp">${res.data.current.temp_c}</div>
     <div class="condition">
-      <img src="https://cdn.weatherapi.com/weather/64x64/day/113.png" alt="Sunny" />
-      <span>Sunny</span>
+      <img src=${res.data.current.condition.icon} alt="" />
+      <span>${res.data.current.condition.text}</span>
     </div>
   </div>`
+}).catch((err) =>{
 
-
-
-
-}).catch(err =>{
-
-console.log(err);
+console.log(  "errr" + err);
 })
 })
 
